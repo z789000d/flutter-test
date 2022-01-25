b8688204d8bb98ba1e3ec673435f465cf1d16195
letao_Wei <>
2022-01-25 17:41:53 +0800

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:untitledtest1/DrawerWidget.dart';
@@ -13,10 +14,14 @@ String exchangeRateJPY = "";
 GlobalKey<ScaffoldState> globalKey = GlobalKey();
 Utillity utillity = Utillity();
 DrawerWidget drawerWidget = DrawerWidget();
+
+String icome = "\$999";
+String expenditure = "\$9,999";
+
 bool hasRecord = false;
 
 void main() {
-  apiPost.post().then((value) {
+  getApi().then((value) {
     exchangeRateJPY = jsonDecode(value.toString())['exchange_rate_JPY'];
 
     runApp(MaterialApp(
@@ -32,6 +37,10 @@ void main() {
       home: HomePage(),
     ));
   });
+}
+
+Future<Response> getApi() async {
+  return apiPost.post();
 }
 
 class HomePage extends StatefulWidget {
@@ -54,37 +63,39 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: PreferredSize(
-            preferredSize: Size.fromHeight(0),
-            child: AppBar(
-              // Here we create one to set status bar color
-              backgroundColor: Colors
-                  .blue, // Set any color of status bar you want; or it defaults to your theme's primary color
-            )),
-        body: Column(
-          children: [
-            homeBar(),
-            Expanded(
-              child: Container(
-                color: Colors.white,
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.vertical,
-                  physics: const BouncingScrollPhysics(
-                      parent: AlwaysScrollableScrollPhysics()),
-                  child: Column(
-                    children: <Widget>[
-                      createItemBar(),
-                      createdDataAndMoneyBar(),
-                      createdContentList(),
-                    ],
-                  ),
+      appBar: PreferredSize(
+          preferredSize: Size.fromHeight(0),
+          child: AppBar(
+            // Here we create one to set status bar color
+            backgroundColor: Colors
+                .blue, // Set any color of status bar you want; or it defaults to your theme's primary color
+          )),
+      body: Column(
+        children: [
+          homeBar(),
+          Expanded(
+            child: Container(
+              color: Colors.white,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                physics: const BouncingScrollPhysics(
+                    parent: AlwaysScrollableScrollPhysics()),
+                child: Column(
+                  children: <Widget>[
+                    createItemBar(),
+                    createdDataAndMoneyBar(),
+                    createdContentList(),
+                  ],
                 ),
               ),
-              flex: 1,
             ),
-            createRefreshButton()
-          ],
-        ));
+            flex: 1,
+          ),
+          createRefreshButton(),
+          createBottomBar()
+        ],
+      ),
+    );
   }
 
   Widget homeBar() {
@@ -183,7 +194,7 @@ class HomePageState extends State<HomePage> {
                 style: TextStyle(color: Colors.white),
               ),
               Text(
-                '99999',
+                icome,
                 style: TextStyle(color: Colors.white),
               )
             ],
@@ -197,7 +208,7 @@ class HomePageState extends State<HomePage> {
                   style: TextStyle(color: Colors.white),
                 ),
                 Text(
-                  '99999',
+                  expenditure,
                   style: TextStyle(color: Colors.white),
                 )
               ],
@@ -211,7 +222,10 @@ class HomePageState extends State<HomePage> {
   Widget createdContentList() {
     List<Widget> datesData = <Widget>[];
     for (int i = 0; i < 100; i++) {
-      datesData.add(Container(child: Text("aaa$i"),margin: EdgeInsets.all(10),));
+      datesData.add(Container(
+        child: Text("aaa$i"),
+        margin: EdgeInsets.all(10),
+      ));
     }
     return Stack(
       children: [
@@ -255,6 +269,125 @@ class HomePageState extends State<HomePage> {
             )),
       ],
     );
+  }
+
+  Widget createBottomBar() {
+    return Container(
+        color: Colors.white,
+        padding: EdgeInsets.only(bottom: 10),
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(bottom: 10),
+              color: Colors.black,
+              width: double.infinity,
+              height: 1,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(left: 10),
+                    child: GestureDetector(
+                      child: Column(
+                        children: [
+                          Icon(
+                            Icons.book,
+                            color: Colors.red,
+                          ),
+                          Text(
+                            '記帳本',
+                            style: TextStyle(color: Colors.red),
+                          )
+                        ],
+                      ),
+                      onTap: (){ print('go 1');},
+                    ),
+                  ),
+                  flex: 1,
+                ),
+                Container(
+                  color: Colors.black,
+                  width: 1,
+                  height: 30,
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(left: 10),
+                    child: GestureDetector(
+                      child: Column(
+                        children: [
+                          Icon(
+                            Icons.wallet_giftcard,
+                            color: Colors.blue,
+                          ),
+                          Text(
+                            '錢包',
+                            style: TextStyle(color: Colors.blue),
+                          )
+                        ],
+                      ),
+                      onTap: (){ print('go 2');},
+                    ),
+                  ),
+                  flex: 1,
+                ),
+                Container(
+                  color: Colors.black,
+                  width: 1,
+                  height: 30,
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(left: 10),
+                    child: GestureDetector(
+                      child: Column(
+                        children: [
+                          Icon(
+                            Icons.analytics_outlined,
+                            color: Colors.blue,
+                          ),
+                          Text(
+                            '分析',
+                            style: TextStyle(color: Colors.blue),
+                          )
+                        ],
+                      ),
+                      onTap: (){ print('go 3');},
+                    ),
+                  ),
+                  flex: 1,
+                ),
+                Container(
+                  color: Colors.black,
+                  width: 1,
+                  height: 30,
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(left: 10),
+                    child: GestureDetector(
+                      child: Column(
+                        children: [
+                          Icon(
+                            Icons.settings,
+                            color: Colors.blue,
+                          ),
+                          Text(
+                            '設定',
+                            style: TextStyle(color: Colors.blue),
+                          )
+                        ],
+                      ),
+                      onTap: (){ print('go 4');},
+                    ),
+                  ),
+                  flex: 1,
+                ),
+              ],
+            ),
+          ],
+        ));
   }
 
   Widget createRefreshButton() {
